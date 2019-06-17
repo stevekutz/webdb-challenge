a457662b8134f73a00ac83f1dd2e18683c52bf99
stevekutz <stkutz@gmail.com>
2019-06-16 23:50:16 -0500

    added context functionality to actions-model

--- actions/actions-model.js
+++ actions/actions-model.js
@@ -21,7 +21,7 @@ function addAction(action) {
       .then(ids => ({id: ids[0]}));
 }
 
-
+/*
 async function getAction(id) {
     const actions =  await db.select('*')
         .from('actions as a')
@@ -34,6 +34,30 @@ async function getAction(id) {
         return actions[0];
     }
 }
+*/
+
+// Now with contexts
+async function getAction(id) {
+    const actions =  await db.select('*')
+        .from('actions as a')
+        .where('a.id', id);
+    // console.log(actions);
+
+    const contexts = await db.select('c.id', 'c.name')
+        .from('actions_contexts as a_c')
+        .join('contexts as c', 'c.id', 'a_c.context_id')
+        .where('a_c.action_id', id)
+        .orderBy('c.id');
+    console.log(">>>>>>>   ", contexts);
+
+    if(actions) {
+        const result = {...actions[0], contexts: contexts};
+        return result;
+    } else {
+        return actions[0];
+    }
+}
+
 
 async function updateAction(id, actionChanges) {
     return db('projects')
--- data/seeds/003-contexts_stretch.js
+++ data/seeds/003-contexts_stretch.js
@@ -1,13 +1,11 @@
 
-exports.seed = function(knex, Promise) {
-  // Deletes ALL existing entries
-  return knex('table_name').del()
-    .then(function () {
-      // Inserts seed entries
-      return knex('table_name').insert([
-        {id: 1, colName: 'rowValue1'},
-        {id: 2, colName: 'rowValue2'},
-        {id: 3, colName: 'rowValue3'}
-      ]);
-    });
+exports.seed = async function(knex) {
+  await knex('contexts').insert([
+        {id: 1, name: 'home'},
+        {id: 2, name: 'lab'},
+        {id: 3, name: 'classroom'},
+        {id: 4, name: 'gym'},
+        {id: 5, name: 'store'}
+  ]);
 };
+
